22ccdc38a3de05c2e8fa7a8822a540181bfd12f1
ShashankBhardwaj113 <98529355+ShashankBhardwaj113@users.noreply.github.com>
2022-02-25 15:52:18 +0530

    deleting es2015

--- shashank codes/es2015/modules_&_classes_i-design_q1/display.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs');
-var data = fs.readFileSync('input.txt').toString().trim().split('\n');
-
-//fill your code
-module.exports.func = function (){
-    return parseInt(data);
-}
