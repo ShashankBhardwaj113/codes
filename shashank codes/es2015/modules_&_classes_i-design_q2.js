d558b55c4f60015c28c26905203c7e1129163353
ShashankBhardwaj113 <98529355+ShashankBhardwaj113@users.noreply.github.com>
2022-02-25 19:43:25 +0530

    Update modules_&_classes_i-design_q2.js

--- shashank codes/es2015/modules_&_classes_i-design_q2.js
+++ shashank codes/es2015/modules_&_classes_i-design_q2.js
@@ -1,37 +1,18 @@
 var fs = require('fs');
-var userData = fs.readFileSync('input.txt').toString().trim().split('\n');
+var input = fs.readFileSync('input.txt').toString().trim().split('\n');
 //fill your code
-let userName = userData[0];
-let userPasswordArray = userData[1].split('');
-
-for(let i=0; i<userPasswordArray.length; i++){
-    let val = userPasswordArray[i];
-    userPasswordArray[i] = String.fromCharCode(val.charCodeAt(0) + 1);
-}
-let userPassword = userPasswordArray.toString().replace(/[,]+/gm,``);
 
 class User {
     //fill your code
-    _encryptedPassword =  '';
-    set writePassword(value){
-        this._encryptedPassword = value;
-    }
-    get showPassword(){
-        return this._encryptedPassword;
-    }
     constructor(name, password){
         this.name = name;
-        this.password = password;
+        this._encryptedPassword = password;
     }
-    login(){
-        User.encryptedPassword = userPassword;
-        let pass = User.encryptedPassword;
-        //console.log(pass + ' ' + this.name + ' ' + this.password);
-        if(this.password == pass){
-            return 'Successfully logged in';
-        }else{
-            return 'Invalid username/password';
-        }
+    set pass(val){
+        this._encryptedPassword = val;
+    }
+    get value(){
+        return this._encryptedPassword;
     }
 }
 
@@ -43,5 +24,19 @@ user[3] = new User("Jessika", "kfttj");
 user[4] = new User("Sarah", "tbsb18");
 
 //fill your code
-
-console.log(user[2].login());
+function getEncrypted(value){
+    var ans = "";
+    for(var i = 0; i<value.length; i++){
+        code = value.charCodeAt(i) + 1;
+        ans += String.fromCharCode(code);
+    }
+    return ans;
+}
+var pasEncrypted = getEncrypted(input[1]);
+for(x of user){
+    if(input[0].trim() === x.name && x.value === pasEncrypted){
+        console.log("Successfully logged in");
+        return;
+    }
+}
+console.log("Invalid username/password");
