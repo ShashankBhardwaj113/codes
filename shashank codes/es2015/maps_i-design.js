4fcde08e3670283fe9babae4234cda3009d90dbd
ShashankBhardwaj113 <98529355+ShashankBhardwaj113@users.noreply.github.com>
2022-02-25 19:14:03 +0530

    Update maps_i-design.js

--- shashank codes/es2015/maps_i-design.js
+++ shashank codes/es2015/maps_i-design.js
@@ -4,7 +4,7 @@ array=array.trim();
 array1=array.split("\n");
 
 let data = array1.toString().replace(/[\r]+/gm,``).split(',');
-console.log(data);
+//console.log(data);
 
 console.log("Menu");
 console.log("1) Add");
@@ -13,3 +13,37 @@ console.log("3) Clear");
 console.log("4) Search");
 console.log("5) Print");
 //Fill code here
+let mySet = new Set();
+for(let i=0; i<data.length; i++){
+    if(data[i] == 1){
+        i++;
+        mySet.add(data[i]);
+        //console.log(data[i]);
+    }
+    else if(data[i] == 2){
+        i++;
+        mySet.delete(data[i]);
+    }
+    else if(data[i] == 3){
+        mySet.clear();
+    }
+    else if(data[i] == 4){
+        i++;
+        if(mySet.has(data[i]))
+        {
+            console.log('Yes');
+        }
+        else{
+            console.log('No');
+        }
+    }
+    else if(data[i] == 5){
+        for(x of mySet){
+            console.log(x);
+        }
+    }
+    else{
+        console.log('Invalid choice');
+    }
+
+}
