7d15d3ca57283a04312da8794dc9e82891b3b45e
ShashankBhardwaj113 <98529355+ShashankBhardwaj113@users.noreply.github.com>
2022-03-01 00:56:28 +0530

    Delete shashank codes/node.js directory

--- shashank codes/node.js/working with node modules/i-assess/question1/app.js
+++
@@ -1,8 +0,0 @@
-// Fill your code here
-var circle = require('./Circle.js');
-
-let radius = process.argv;
-radius[2] = 5;
-
-console.log('Area of the circle is ' + circle.computeArea(radius[2]).toFixed(2));
-console.log('Circumference of the circle is ' + circle.computeCircumference(radius[2]).toFixed(2));
--- shashank codes/node.js/working with node modules/i-assess/question3/app.js
+++
@@ -1,8 +0,0 @@
-//Fill your code
-var math = require('mathjs');
-
-let command = process.argv.slice(2);
-
-//command[0] = 3+4*2; => 11
-
-console.log(math.evaluate(command[0]));
