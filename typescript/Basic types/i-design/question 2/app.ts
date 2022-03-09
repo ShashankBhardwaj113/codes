f654181cca3536465af9bd9946aa4d007e7e49f0
ShashankBhardwaj113 <98529355+ShashankBhardwaj113@users.noreply.github.com>
2022-03-09 16:20:19 +0530

    Delete typescript/Basic types directory

--- typescript/Basic types/i-design/question 2/app.ts
+++
@@ -1,7 +0,0 @@
-let require:any;
-let fs=require("fs");
-fs.readFile("input.txt","utf-8",function(err,data){
-    let line=data.split("\n");
-    //Fill your code here
-    console.log("Welcome " + line[0]+ " " + line[1] + " :-)");
-})
--- typescript/Basic types/i-design/question 6/app.ts
+++
@@ -1,7 +0,0 @@
-let require:any;
-let fs=require("fs");
-fs.readFile("input.txt","utf-8",function(err,data){
-    // Fill your code here
-    data = parseFloat(data);
-    console.log(data.toExponential(2));
-})
