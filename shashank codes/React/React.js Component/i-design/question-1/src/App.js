3b88f338451da27782a36f6ac374d6d2e860e9f7
ShashankBhardwaj113 <98529355+ShashankBhardwaj113@users.noreply.github.com>
2022-04-05 17:20:04 +0530

    Update App.js

--- shashank codes/React/React.js Component/i-design/question-1/src/App.js
+++ shashank codes/React/React.js Component/i-design/question-1/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Profile from './Profile';
 
-function App() {
-  const data = {
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
       title:'My Profile',
       profileicon:require('./image/profileicon.png'),
       name:'William',
@@ -10,19 +12,65 @@ function App() {
       mobilenumber:'8976541234',
       age:27,
       address:'Great Street,London'
+    }
   }
 
-  return (
-    <Profile
-    title={data.title}
-    profileicon={data.profileicon}
-    age={data.age}
-    name={data.name}
-    email={data.email}
-    address={data.address}
-    mobilenumber={data.mobilenumber}
-    />
-  );
+  render(){
+    return (
+      <Profile title={this.state.title} profileicon={this.state.profileicon} >
+        <table style={{marginLeft: 'auto', marginRight: 'auto'}}>
+                                    <thead>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                    <td>Name : </td>
+                                    <td>{this.state.name}</td>
+                                    </tr>
+                                    <tr>
+                                    <td>Email : </td>
+                                    <td>{this.state.email}</td>
+                                    </tr>
+                                    <tr>
+                                    <td>Mobile : </td>
+                                    <td>{this.state.mobilenumber}</td>
+                                    </tr>
+                                    <tr>
+                                    <td>Age : </td>
+                                    <td>{this.state.age}</td>
+                                    </tr>
+                                    <tr>
+                                    <td>Address : </td>
+                                    <td>{this.state.address}</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+      </Profile>
+    );
+  }
 }
+
+// function App() {
+//   const data = {
+//       title:'My Profile',
+//       profileicon:require('./image/profileicon.png'),
+//       name:'William',
+//       email:'william@gmail.com',
+//       mobilenumber:'8976541234',
+//       age:27,
+//       address:'Great Street,London'
+//   }
+
+//   return (
+//     <Profile
+//     title={data.title}
+//     profileicon={data.profileicon}
+//     age={data.age}
+//     name={data.name}
+//     email={data.email}
+//     address={data.address}
+//     mobilenumber={data.mobilenumber}
+//     />
+//   );
+// }
 
 export default App;
